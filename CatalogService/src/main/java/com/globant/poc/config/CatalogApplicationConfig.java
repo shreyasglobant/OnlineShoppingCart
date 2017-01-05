afe4ace3352a82ed7a034d50043ae1a639d2ac51
patild2 <divakar.patil@emc.com>
2017-01-05 12:26:40 +0530

    Added catalog service

---
+++ CatalogService/src/main/java/com/globant/poc/config/CatalogApplicationConfig.java
@@ -0,0 +1,30 @@
+package com.globant.poc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//Defines this application as a Spring Boot Application.
+@EnableAutoConfiguration
+//Microservice registration and discovery enabled.
+@EnableDiscoveryClient
+public class CatalogApplicationConfig {
+
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+	
+	public static void main(String[] args) {
+		//Custom Config file name for the application. By 
+		//By default Spring Boot applications look for an application.properties
+		//or application.yml file for configuration.
+		//Here it will check for orders-config.yml or orders-config.properties
+		System.setProperty("spring.config.name", "catalog-config");
+		
+		SpringApplication.run(CatalogApplicationConfig.class, args);
+
+	}
+
+}
